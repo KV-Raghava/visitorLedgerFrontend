bc6a5afa49a2f69d8cd8ee330883d1f88316b621
Venkata Raghava Kuruva <venkata.kuruva@olamnet.com>
2023-07-10 13:09:29 +0530

    first commit

---
+++ src/app/modules/dashboard/Filter/changePassword/change-password/change-password.component.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css']
+})
+export class ChangePasswordComponent {
+constructor(private http:HttpClient){}
+async onSubmit(formData : any){
+  console.log(formData.value);
+  await this.http.put(`http://localhost:8050/admin/change/${formData.value.user}/${formData.value.curr}/${formData.value.new}/${formData.value.confirm}`,{}).subscribe(data => console.log(data));
+  alert("Password Changed Successfully!");
+  formData.resetForm();
+}
+}
